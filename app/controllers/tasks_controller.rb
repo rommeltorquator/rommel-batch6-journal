9acef09e71e79cd50e08ca6520c8e6132f264cc4
Rommel Torquator <rommeltorquator09@gmail.com>
2021-05-19 10:35:52 -0400

    Finished the CRUD functionality of task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,44 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
   end
 
   def create
-    
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])    
+  end
+
+  def update
+    @task = Task.find(params[:id])   
+
+    @task.update(task_params)    
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :description, :deadline)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
-  
-  get 'tasks/index'
 
-  get 'home/index'
+  resources :tasks, only: [ :index, :new, :create, :show, :edit, :update, :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
