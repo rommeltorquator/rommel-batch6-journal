75fe6010f048b52b11feed723526f3a4ce7bc673
Rommel Torquator <rommeltorquator09@gmail.com>
2021-05-20 08:17:14 -0400

    Enhanced the layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
 
-  resources :tasks, only: [ :index, :new, :create, :show, :edit, :update, :destroy ]
+  # resources :tasks, only: [ :index, :new, :create, :show, :edit, :update, :destroy ]
+  resources :tasks
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
