193a8c9e78c358c46850944fe0ea57caa49ac7e6
Rommel Torquator <rommeltorquator09@gmail.com>
2021-05-27 09:27:56 -0400

    Added modal on Categories index page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -27,6 +27,13 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select 'img'
   end
 
+  test "should show the navigation" do
+    get root_path
+    assert_response :success
+
+    assert_select 'nav'
+  end
+
   # assert_select 'nav.side_nav a', minimum: 4
   #   assert_select 'main ul.catalog li', 3
   #   assert_select 'h2', 'Programming Ruby 1.9'
