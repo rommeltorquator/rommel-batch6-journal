cd4d6ba5464d27a66978300c162cc626246cb006
Rommel Torquator <rommeltorquator09@gmail.com>
2021-06-08 21:23:52 -0400

    deploying to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root "home#index"
   resources :categories
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'sign_up' }
 
-  root "home#index"
   get 'dashboard', to: 'home#dashboard'
 
   resources :tasks, only: [ :create, :edit, :destroy, :update ] do
