9790e57101e4bc6585f833e858c132b355135dec
Rommel Torquator <rommeltorquator09@gmail.com>
2021-06-08 23:31:04 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'sign_up' }
 
   get 'dashboard', to: 'home#dashboard'
+  get 'dummy', to: 'home#dummy'
 
   resources :tasks, only: [ :create, :edit, :destroy, :update ] do
     collection do
